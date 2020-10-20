897192dcd6db7a62053972eb4b886d9f764c55e4
Michele Ambrosio <michele.ambrosio@eduzz.com>
2020-10-20 12:57:47 -0300

    Transactions dashboard and import csv files

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -1,16 +1,11 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
-
-import filesize from 'filesize';
-
-import Header from '../../components/Header';
-import FileList from '../../components/FileList';
-import Upload from '../../components/Upload';
-
-import { Container, Title, ImportFileContainer, Footer } from './styles';
-
 import alert from '../../assets/alert.svg';
+import FileList from '../../components/FileList';
+import Header from '../../components/Header';
+import Upload from '../../components/Upload';
 import api from '../../services/api';
+import { Container, Footer, ImportFileContainer, Title } from './styles';
 
 interface FileProps {
   file: File;
@@ -23,19 +18,29 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
-
-    // TODO
+    const data = new FormData();
+    data.append('file', uploadedFiles[0].file);
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
+      history.push('/');
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const newUploadedFiles: FileProps[] = [];
+
+    files.map(file => {
+      newUploadedFiles.push({
+        file: file,
+        name: file.name,
+        readableSize: file.size.toString() + ' bytes'
+      });
+    });
+
+    setUploadedFiles(newUploadedFiles);
   }
 
   return (
